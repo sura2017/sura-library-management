53f6af2e29381a617692daf967aade0b479f05df
Sura207 <suraabraham12@gmail.com>
2026-04-13 23:29:15 -0700

    feat: integrated Nodemailer for automated loan notifications

---
+++ server/utils/emailService.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+const sendEmail = (to, subject, text) => {
+    const mailOptions = {
+        from: `"Sura Library" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        text
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log("❌ Email Error:", error);
+        } else {
+            console.log("📧 Email sent successfully to: " + to);
+        }
+    });
+};
+
+module.exports = sendEmail;
